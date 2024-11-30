36ece187cd38c061bd0da1a1e395ad91ee13377e
Acueres <frosteagle97@gmail.com>
2024-11-30 18:43:51 +0300

    Add Vs Code config

--- Test/LexerTest.cs
+++ Test/LexerTest.cs
@@ -110,10 +110,10 @@ namespace Test
             tokens = tokens.Where(t => t.Type != TokenType.Semicolon && t.Type != TokenType.EOF).ToArray();
 
             Assert.Equal(3, tokens.Length);
-            Assert.Equal("multiline string\r\n", tokens[0].Value);
+            Assert.Equal("multiline string\n", tokens[0].Value);
             Assert.Equal("regular string", tokens[1].Value);
             Assert.Equal(2, tokens[1].Line);
-            Assert.Equal("another 'multiline' string\r\n text\r\n", tokens[2].Value);
+            Assert.Equal("another 'multiline' string\n text\n", tokens[2].Value);
             Assert.Equal(4, tokens[2].Line);
         }
 
